5442c64db7e728e0bfd7260bba93739ccbb111ae
yussef <yussefgafar@gmail.com>
2024-10-30 02:50:07 +0200

    رسالة توضيح التحديثات

--- main.js
+++ main.js
@@ -1,14 +1,32 @@
-async function readFileContent(filePath) {
-  try {
-    const response = await fetch(filePath);
-    if (!response.ok) {
-      throw new Error('لم يتم العثور على الملف');
-    }
-    const data = await response.text();
-    return data;
-  } catch (error) {
-    console.error(error.message);
-    return null;
-  }
-}
+function start() {
+  /*document.body.innerHTML += `  
+    <div id="msg-div">
+      <p id="msg"></p>
+    </div>`;*/
 
+  let azkar = [
+        'الله اكبر',
+        'الصلاه اهم من كل شئ',
+        'صلي علي النبي',
+        'لا إلاه إلا الله',
+        'بإسم الله تتم الامور',
+        'لا إلاه الا انت سبحانك',
+        'المُتوفيِ هو الله\n المُتوفيَ هو الانسان الميت',
+        'إن الله يراك',
+        'لا تنسي صلاتك',
+        'لا تدع الهاتف يلهيك عن صلاتك',
+        'اذكر الله',
+        'سبحان الله',
+        'بسم الله',
+        'لا إله إلا الله',
+        'صلي علي محمد',
+        'محمد اخر الانبياء',
+        'لم يذكر كل الانبياء قي القرأن',
+        'سيدنا ابراهيم ابو الانبياء\n فكل الانبياء من بعده كانو من نسله'
+        ]
+
+  const msgDiv = document.getElementById('msgDiv');
+  const msg = document.getElementById('msg');
+  msg.innerText = azkar[Math.floor(Math.random() * azkar.length)];
+}
+start();
